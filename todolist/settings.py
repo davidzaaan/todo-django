6ac963180245f605405540389934aa75ae4d23b7
david <odmh2711@gmail.com>
2022-01-21 13:02:36 -0500

    Updated settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s9n^ttak#g67^u$#p-bisys+-k&wwvllilci+gdwmcztsoiq*z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
