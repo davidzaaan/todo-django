d5285a7c237bee1765ff30ce12010fff71936551
davidzaaan <79036402+davidzaaan@users.noreply.github.com>
2022-02-19 20:25:06 -0500

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -6,7 +6,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .greeting import get_greeting_time, get_location
-import inspect
 
 
 
